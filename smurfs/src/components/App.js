0bc710719d172f57f9df7690338037544bcde3aa
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-26 13:33:30 -0400

    Added some styling

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -13,14 +13,15 @@ const App = () => {
           <h1>SMURFS! 2.0 W/ Redux</h1>
           <div>Welcome to your state management version of Smurfs!</div>
           <div>Have fun!</div>
+          <Form />
           <button onClick={() => dispatch(fetchData())}>Fetch Smurfs!</button>
+
           {state.isLoading && <p>Loading your Smurfs!</p>}
         <div>
           {state.smurfs.map(i => (
           <h4 key={i.name}>{i.name}, {i.age}, {i.height}</h4>
           ))}
         </div>
-        <Form />
       </div>
     );
   }
